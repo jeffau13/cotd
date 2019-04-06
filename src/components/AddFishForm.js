aaeca2db0c265b5d407bb5c5f71107cf257d148b
jeffau13 <jeffau13@gmail.com>
2019-04-05 23:02:48 -0400

    added AddFishForm component

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+export default class AddFishForm extends Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = event => {
+    event.preventDefault();
+    const fish = {
+      nameRef: this.nameRef.current.value,
+      priceRef: parseFloat(this.priceRef.current.value),
+      statusRef: this.statusRef.current.value,
+      descRef: this.descRef.current.value,
+      imageRef: this.imageRef.current.value
+    };
+    this.props.addFish(fish);
+  };
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input
+          name="name"
+          type="text"
+          ref={this.nameRef}
+          placeholder="Name"
+          autoComplete="off"
+        />
+        <input
+          name="price"
+          type="text"
+          ref={this.priceRef}
+          placeholder="Price"
+          autoComplete="off"
+        />
+        <select name="status" ref={this.statusRef} type="text">
+          <option value="available">Fresh</option>
+          <option value="unavailable">Sold Out</option>
+        </select>
+        <textarea name="desc" ref={this.descRef} placeholder="Desc" />
+        <input
+          name="imgage"
+          type="text"
+          ref={this.imageRef}
+          placeholder="Image URL"
+          autoComplete="off"
+        />
+        <button type="submit">Add Fish</button>
+      </form>
+    );
+  }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,20 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 export default class App extends Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  addFish = fish => {
+    // update state:
+    // 1.take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // 2. add new fish to that fishes variable (unique)
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. Set the new fishes object ot state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +26,7 @@ export default class App extends Component {
           {/* <Fish /> */}
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
