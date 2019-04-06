d588256128d5986ae5ba62fb04344738e2c4658c
jeffau13 <jeffau13@gmail.com>
2019-04-06 18:27:03 -0400

    implemented persistent state with real time firebase db

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,25 @@ import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 export default class App extends Component {
   state = {
     fishes: {},
     order: {}
   };
+
+  componentDidMount() {
+    const { params } = this.props.match;
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
   addFish = fish => {
     // update state:
     // 1.take a copy of the existing state
