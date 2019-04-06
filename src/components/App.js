8e0df2fb111ee9320c7926dd971bb43927f4d519
jeffau13 <jeffau13@gmail.com>
2019-04-06 17:48:49 -0400

    added add to order functionality

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,15 @@ export default class App extends Component {
     this.setState({ fishes: sampleFishes });
   };
 
+  addToOrder = key => {
+    //1. take a copy of existing state of order
+    const order = { ...this.state.order };
+    //2. add to order or update order
+    order[key] = order[key] + 1 || 1;
+    //3. call setState
+    this.setState({ order });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -30,12 +39,17 @@ export default class App extends Component {
           <Header tagline="Fresh Seafood Daily" />
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
           {/* <Fish /> */}
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
